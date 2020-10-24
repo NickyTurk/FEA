f0f1e565e88c7810e67428b448e68ab20bfda3a9
Amy <amy.peerlinck@student.montana.edu>
2020-10-24 11:05:26 -0600

    results

--- test.py
+++ test.py
@@ -73,8 +73,8 @@ if __name__ == '__main__':
 
     benchmark = args.benchmark
 
-    functions = [F3, F5, F9, F15, F19]  # [F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12, F13, F14, F15, F16, F17, F18, F19,F20] _benchmarks[benchmark]["function"]
-    function_names = ['F3', 'F5', 'F9', 'F15', 'F20']  # ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20']
+    functions = [F3, F5, F9, F19]  # [F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12, F13, F14, F15, F16, F17, F18, F19,F20] _benchmarks[benchmark]["function"]
+    function_names = ['F3', 'F5', 'F9', 'F20']  # ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20']
 
     no_m_param = ['F1', 'F2', 'F3', 'F19', 'F20']
     shifted_error_function = ['F14', 'F15', 'F16']
@@ -86,8 +86,8 @@ if __name__ == '__main__':
     # test_diff_grouping(4, functions, function_names)
 
     dimensions = [50, 100]
-    file_extension = "m4_diff_grouping"
-    # file_extension = "overlapping_diff_grouping"
+    # file_extension = "m4_diff_grouping"
+    file_extension = "overlapping_diff_grouping"
     filename_list = get_files_list("F*_" + file_extension + "_small_epsilon.csv")
 
     for filename in filename_list:
