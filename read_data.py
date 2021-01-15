daafca0695ab22ee639e45d5bc381f3d378b93f6
AmyLinck <amy.peerlinck@student.montana.edu>
2021-01-15 15:50:50 +0000

    F19 results

--- read_data.py
+++ read_data.py
@@ -36,7 +36,7 @@ def get_files_list(file_regex, subfolder = ''):
 
 def import_single_function_factors(file_name, dim=50, epsilon=0):
 
-    frame = pd.read_csv('results/'+file_name, header=0)
+    frame = pd.read_csv(file_name, header=0)
     frame.columns = map(str.upper, frame.columns)
     frame = frame.rename(columns = {"DIM": "DIMENSION"}, errors="ignore")
     dim_frame = frame.loc[frame['DIMENSION'] == int(dim)]
