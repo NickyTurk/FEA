103bd1cab4f6d6c6eec77c49c9c1fcd563bfbaf4
Elliott Pryor <pryorelliott@gmail.com>
2021-02-25 08:54:39 -0700

    F3, F5, F11

--- testing_script.py
+++ testing_script.py
@@ -84,24 +84,26 @@ class TestOptimization():
         return summary
 
 if __name__ == '__main__':
-    function_nrs = [3,5,11,17,19]
+    #F5, F11, F17, F19
+    function_nrs = [11]
 
-    for nr in function_nrs:
-        test_opt = TestOptimization(dim=20, function_number=nr, factor_topology='MEET', DG_epsilon=0)
-        '''
-        with open('results/pso_20/' + str(test_opt.function_name) + '_pso_param.csv', 'a') as write_to_csv:
-            csv_writer = csv.writer(write_to_csv)
-            csv_writer.writerow(['function', 'dim', 'population', 'iterations', 'fitnesses', 'stats'])
-            for pop in [500]:
+    for itr in range(9):
+        for nr in function_nrs:
+            test_opt = TestOptimization(dim=20, function_number=nr, factor_topology='MEET', DG_epsilon=0)
+            '''
+            with open('results/pso_20/' + str(test_opt.function_name) + '_pso_param.csv', 'a') as write_to_csv:
+                csv_writer = csv.writer(write_to_csv)
+                csv_writer.writerow(['function', 'dim', 'population', 'iterations', 'fitnesses', 'stats'])
+                for pop in [500]:
+                    print('function nr: ', nr)
+                    summary = test_opt.test_pso(pop, 200)
+                    to_write = [str(test_opt.function_name), str(test_opt.dim), str(pop), str(200), summary["fitnesses"], summary["statistics"]]
+                    csv_writer.writerow(to_write)
+            
+            '''
+            with open('results/FEA_PSO/' + str(test_opt.function_name) + '_dim' + str(test_opt.dim) + test_opt.file_extension + ".csv", 'a') as write_to_csv:
                 print('function nr: ', nr)
-                summary = test_opt.test_pso(pop, 200)
-                to_write = [str(test_opt.function_name), str(test_opt.dim), str(pop), str(200), summary["fitnesses"], summary["statistics"]]
-                csv_writer.writerow(to_write)
-        
-        ''' 
-        with open('results/FEA_PSO/' + str(test_opt.function_name) + '_dim' + str(test_opt.dim) + test_opt.file_extension + ".csv", 'a') as write_to_csv:
-            print('function nr: ', nr)
-            summary = test_opt.test_fea(pso_iterations=10, pop=500, fea_iterations=10)
-            csv_writer = csv.writer(write_to_csv)
-            csv_writer.writerow(summary["fitnesses"])
+                summary = test_opt.test_fea(pso_iterations=10, pop=500, fea_iterations=10)
+                csv_writer = csv.writer(write_to_csv)
+                csv_writer.writerow(summary["fitnesses"])
         
