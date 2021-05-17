fixed bug in compete step. (incorrect value initialization)

refactoring/FEA/factorevolution.py
@@ -32,7 +32,7 @@ class FEA:
         """
         Construct new global solution based on best shared variables from all swarms
         """
-        gs = [x for x in self.global_solution]
+        gs = self.global_solution
         print('global fitness found: ', self.global_fitness)
         print('===================================================')
         for alg in self.subpopulations:
@@ -56,7 +56,7 @@ class FEA:
         f = self.f
         curr_fitness = f.run(self.global_solution)
         for var_idx in range(self.dim):
-            best_value_for_var = 0
+            best_value_for_var = sol[var_idx]
             for pop_idx in self.factor_architecture.optimizers[var_idx]:
                 curr_pop = self.subpopulations[pop_idx]
                 pop_var_idx = np.where(curr_pop.factor == var_idx)

refactoring/baseAlgorithms/pso.py
@@ -123,12 +123,12 @@ class PSO(object):
 
     def replace_worst_solution(self, global_solution):
         # find worst particle
-        self.global_solution = global_solution
+        self.global_solution = np.array([x for x in global_solution])
         worst_particle = max(self.pop)
         worst_particle_index = [i for i, x in enumerate(self.pop) if x == worst_particle]
         partial_solution = [x for i, x in enumerate(global_solution) if i in self.factor] # if i in self.factor
         self.pop[worst_particle_index[0]].set_position(partial_solution)
-        self.pop[worst_particle_index[0]].set_fitness(self.f.run(global_solution))
+        self.pop[worst_particle_index[0]].set_fitness(self.f.run(self.global_solution))
 
     def run(self):
         for i in range(self.generations):