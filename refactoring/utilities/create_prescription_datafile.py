f57fd384ca2e1f917d774f8491b4e74991598609
amylinck <amy.peerlinck@student.montana.edu>
2021-08-05 06:07:34 -0600

    checking field  info

--- refactoring/utilities/create_prescription_datafile.py
+++ refactoring/utilities/create_prescription_datafile.py
@@ -10,19 +10,19 @@ import re
 import pandas as pd
 import numpy as np
 
-experiment_filenames = ["../../results/FEAMOO/NSGA2_Sec35Mid_trial_3_objectives_ga_runs_200_population_500_2807110338.pickle", "../../results/FEAMOO/NSGA2_Sec35West_trial_3_objectives_ga_runs_200_population_500_2807110402.pickle"] #, "../../results/FEAMOO/FEAMOO_Sec35West_trial_3_objectives_linear_topo_ga_runs_100_population_200_1707230035.pickle"]
-aggregated_data_files = ["../../../Documents/Work/OFPE/Data/Sec35Mid/broyles_sec35mid_10m_yld_2016-2020_UPDATE.csv", "../../../Documents/Work/OFPE/Data/Sec35West/broyles_sec35west_10m_yld_2016-2020_UPDATE.csv"]
-field_files= [ "../utilities/saved_fields/sec35mid.pickle", "../utilities/saved_fields/sec35west.pickle"]
+experiment_filenames = ["../../results/FEAMOO/CCEAMOO_Sec35Mid_trial_3_objectives_strip_topo_ga_runs_100_population_500_2207202949.pickle", "../../results/FEAMOO/CCEAMOO_Sec35West_trial_3_objectives_strip_topo_ga_runs_100_population_500_2307190202.pickle"] #"../../results/FEAMOO/CCEAMOO_Henrys_trial_3_objectives_strip_topo_ga_runs_100_population_500_2007202659.pickle"
+aggregated_data_files = ["../../../Documents/Work/OFPE/Data/Sec35Mid/broyles_sec35mid_10m_yld_2016-2020_UPDATE.csv", "../../../Documents/Work/OFPE/Data/Sec35West/broyles_sec35west_10m_yld_2016-2020_UPDATE.csv"] #"../../../Documents/Work/OFPE/Data/Henrys/wood_henrys_10m_yld_2016-2020_UPDATE.csv"]
+field_files= ["../utilities/saved_fields/sec35mid.pickle", "../utilities/saved_fields/sec35west.pickle"] #"../utilities/saved_fields/Henrys.pickle"
 
 def create_pd_dataframe(prescription, field, headers, dps):
     all_points_df = pd.DataFrame()
-    project_from_latlong = Transformer.from_crs(field.latlong_crs, field.aa_crs)
+    project_from_latlong = Transformer.from_crs(field.latlong_crs, 'epsg:32612')#'epsg:32612')
     for i, gridcell in enumerate(prescription.variables):
         x_int = headers['x']
         y_int = headers['y']
-        print('point: ', dps[:,x_int], dps[:,y_int])
+        # print('point: ', dps[:,x_int], dps[:,y_int])
         bl_x, bl_y = gridcell.bottomleft_x, gridcell.bottomleft_y
-        print('cell: ',bl_x, bl_y)
+        # print('cell: ',bl_x, bl_y)
         ur_x, ur_y = gridcell.upperright_x, gridcell.upperright_y
         points_in_cell = dps[(dps[:,y_int] >= bl_x) &
                                             (dps[:,y_int] <= ur_x) &
@@ -41,18 +41,18 @@ def create_pd_dataframe(prescription, field, headers, dps):
 
 for agg_file, experiment, fieldfile in zip(aggregated_data_files, experiment_filenames, field_files):
     feamoo = pickle.load(open(experiment, 'rb'))
-    # print(feamoo.iteration_stats[-1]['diversity'])
+    print(feamoo.iteration_stats[-1])
     field = pickle.load(open(fieldfile, 'rb'))
+    print(field.nitrogen_list)
     field_name = re.search(r'.*\/FEAMOO\/(.*)_trial',experiment)
     objectives = ['jumps', 'strat', 'fertilizer_rate']
     df = pd.read_csv(agg_file)
-    print(df['x'])
+    # print(df['x'])
     print(field.latlong_crs, field.aa_crs, field.field_crs)
     # if field.aa_crs != field.latlong_crs:
-    project_to_latlong = Transformer.from_crs('epsg:26918', field.latlong_crs) #32629
+    project_to_latlong = Transformer.from_crs('epsg:32612', field.latlong_crs) #32629, 32612 # 'epsg:32612'
     xy = np.array([np.array(project_to_latlong.transform(x, y)) for x, y in zip(df['x'], df['y'])])
     df['x'] = xy[:, 0]
-    print(df['x'])
     df['y'] = xy[:, 1]
     headers = {c: i for i, c in enumerate(df.columns)}
     header_index = {i:c for i,c in enumerate(df.columns)}
@@ -61,7 +61,7 @@ for agg_file, experiment, fieldfile in zip(aggregated_data_files, experiment_fil
     find_center_obj = []
     for obj in objectives:
         filename_to_write = '../../MOO_final_prescriptions/' + field_name.group(1) + '_prescription_' + obj + '_objective_runs_' + str(
-            feamoo.ga_runs) + '_pop_' + str(feamoo.population_size) + '_UPDATED.csv'
+            feamoo.base_alg_iterations) + '_pop_' + str(feamoo.pop_size) + '_UPDATED.csv'
         feamoo.nondom_archive.sort(key=attrgetter(obj))
         prescription = feamoo.nondom_archive[0]
         find_center_obj.append(np.array(prescription.objective_values))
@@ -81,6 +81,6 @@ for agg_file, experiment, fieldfile in zip(aggregated_data_files, experiment_fil
     prescription = feamoo.nondom_archive[idx]
     print(point, prescription.objective_values)
     filename_to_write = '../../MOO_final_prescriptions/' + field_name.group(1) + '_prescription_center_objective_runs_' + str(
-            feamoo.ga_runs) + '_pop_' + str(feamoo.population_size) + '_UPDATED.csv'
+            feamoo.base_alg_iterations) + '_pop_' + str(feamoo.pop_size) + '_UPDATED.csv'
     all_points_df = create_pd_dataframe(prescription, field, headers, dps)
     all_points_df.to_csv(filename_to_write)
