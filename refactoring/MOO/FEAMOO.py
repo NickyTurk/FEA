Bug fixed fitness score calculation

refactoring/MOO/FEAMOO.py
@@ -11,8 +11,9 @@ import random
 
 class FEAMOO:
     def __init__(self, fea_iterations, alg_iterations, pop_size, fa, base_alg, dimensions,
-                 combinatorial_options=[], ref_point = [1,1,1]):
+                 combinatorial_options=[], upper_value_limit=200, ref_point=[1, 1, 1]):
         self.combinatorial_options = combinatorial_options
+        self.upper_value_limit = upper_value_limit
         self.dim = dimensions
         self.nondom_archive = []
         self.population = []
@@ -30,13 +31,18 @@ class FEAMOO:
         self.iteration_stats = []
 
     def initialize_moo_subpopulations(self):
-        random_global_variables = random.choices(self.combinatorial_options, k=self.dim)
-        #print('global vars: ', random_global_variables)
+        if self.combinatorial_options:
+            random_global_variables = random.choices(self.combinatorial_options, k=self.dim)
+        else:
+            random_global_variables = [random.randrange(0, self.upper_value_limit) for x in range(self.dim)]
+        print('global vars: ', random_global_variables)
         objs = self.base_algorithm(dimensions=self.dim).calc_fitness(random_global_variables)
         random_global_solution = PopulationMember(random_global_variables, objs)
         self.global_solutions.append(random_global_solution)
-        return [self.base_algorithm(ea_runs=self.base_alg_iterations, dimensions=len(factor), combinatorial_values=self.combinatorial_options, population_size=self.pop_size, factor=factor,
-                    global_solution=random_global_solution) for factor in self.factor_architecture.factors]
+        return [self.base_algorithm(ea_runs=self.base_alg_iterations, dimensions=len(factor),
+                                    combinatorial_values=self.combinatorial_options, upper_value_limit=self.upper_value_limit,
+                                    population_size=self.pop_size, factor=factor,
+                                    global_solution=random_global_solution) for factor in self.factor_architecture.factors]
 
     def update_archive(self):
         nondom_indeces = find_non_dominated(np.array([np.array(x.fitness) for x in self.nondom_archive]))
@@ -65,6 +71,7 @@ class FEAMOO:
         while fea_run != self.fea_runs:  # len(change_in_nondom_size) < 4 and
             for alg in self.subpopulations:
                 alg.run(fea_run=fea_run)
+                print('subpopulation done')
             self.compete()
             self.share_solution()
             self.update_archive()
@@ -73,13 +80,13 @@ class FEAMOO:
             else:
                 change_in_nondom_size = []
             # [print(x.fitness) for x in self.nondom_archive]
-            print(self.nondom_archive[-1].fitness)
+            print("last nondom solution: ", self.nondom_archive[-1].fitness)
             old_archive_length = len(self.nondom_archive)
             eval_dict = self.po.evaluate_solution(self.nondom_archive, self.worst_fitness_ref)
             eval_dict['FEA_run'] = fea_run
             eval_dict['ND_size'] = len(self.nondom_archive)
             self.iteration_stats.append(eval_dict)
-            print(eval_dict)
+            print("eval dict", eval_dict)
             # [print(s.objective_values) for s in self.nondom_archive]
             # [print(i, ': ', s.objective_values) for i,s in enumerate(self.iteration_stats[fea_run+1]['global solutions'])]
             fea_run = fea_run+1

refactoring/basealgorithms/ga.py
@@ -11,7 +11,8 @@ import random
 
 class GA:
     def __init__(self, dimensions=100, population_size=200, tournament_size=5, mutation_rate=0.2, crossover_rate=0.95,
-                 ga_runs=100, mutation_type="multi bitflip", crossover_type="single", offspring_size=100):
+                 ga_runs=100, mutation_type="multi bitflip", crossover_type="single", offspring_size=100,
+                 continuous_var_space=False, upper_value_limit=200):
         """
         @param dimensions: Integer. Number of variables in a single individual.
         @param population_size: Integer. Number of individuals to form the population in a single generation.
@@ -20,7 +21,7 @@ class GA:
         @param crossover_rate: Float between 0.0 and 1.0. Chance of performing crossover of the selected parent pair.
         @param ga_runs: Integer. How many generations to run the GA for.
         @param mutation_type: String 'swap', 'scramble', 'multi bitflip' or 'single bitflip'. Which type of mutation to perform.
-        @param crossover_type: String 'single', or 'multi'. Which type of crossover to perform.
+        @param crossover_type: String 'single', 'uniform', or 'multi'. Which type of crossover to perform.
         @param offspring_size: Integer. How many children do you wish to create each generation.
         """
         self.name = 'genetic algorithm'
@@ -33,6 +34,8 @@ class GA:
         self.ga_runs = ga_runs
         self.mutation_type = mutation_type
         self.crossover_type = crossover_type
+        self.continuous_bool = continuous_var_space
+        self.upper_value_limit = upper_value_limit
 
     def tournament_selection(self, population):
         """
@@ -69,47 +72,49 @@ class GA:
         @return: The mutated child or a copy of the original solution if mutation was not performed.
         """
         _solution = [x for x in original_solution]
-        rand = random.random()
-        if rand < self.mutation_rate:
-            if self.mutation_type == "swap":
-                numbers = list(range(0, self.dimensions))
-                index_1 = random.choice(numbers)
-                numbers.remove(index_1)
-                index_2 = random.choice(numbers)
-                _solution[index_1] = original_solution[index_2]
-                _solution[index_2] = original_solution[index_1]
+        if self.continuous_bool:
+            for i in range(len(_solution)):
+                if random.random() < self.mutation_rate:
+                    _solution[i] = random.uniform(0, self.upper_value_limit)
+        else:
+            if random.random() < self.mutation_rate:
+                if self.mutation_type == "swap":
+                    numbers = list(range(0, self.dimensions))
+                    index_1 = random.choice(numbers)
+                    numbers.remove(index_1)
+                    index_2 = random.choice(numbers)
+                    _solution[index_1] = original_solution[index_2]
+                    _solution[index_2] = original_solution[index_1]
 
-            elif self.mutation_type == "scramble":
-                index_1 = random.choice(list(range(0, self.dimensions)))
-                numbers = list(range(0, self.dimensions))  # numbers to choose next index from
-                numbers.remove(index_1)  # remove already chosen index from these numbers
-                index_2 = random.choice(numbers)
-                if index_1 > index_2:
-                    max_index = index_1
-                    min_index = index_2
-                else:
-                    max_index = index_2
-                    min_index = index_1
-                temp_list = original_solution[min_index:max_index]
-                np.random.shuffle(temp_list)
-                _solution[min_index:max_index] = temp_list
-            elif self.mutation_type == "single bitflip":
-                numbers = list(range(0, self.dimensions))
-                index_1 = random.choice(numbers)
-                temp = _solution[index_1]
-                if temp == 0:
-                    _solution[index_1] = 1
-                elif temp ==1:
-                    _solution[index_1] = 0
-            elif self.mutation_type == "multi bitflip":
-                for i, x in enumerate(_solution):
-                    rand = random.random()
-                    if rand < self.mutation_rate:
-                        if x == 0:
-                            _solution[i] = 1
-                        elif x == 1:
-                            _solution[i] = 0
-
+                elif self.mutation_type == "scramble":
+                    index_1 = random.choice(list(range(0, self.dimensions)))
+                    numbers = list(range(0, self.dimensions))  # numbers to choose next index from
+                    numbers.remove(index_1)  # remove already chosen index from these numbers
+                    index_2 = random.choice(numbers)
+                    if index_1 > index_2:
+                        max_index = index_1
+                        min_index = index_2
+                    else:
+                        max_index = index_2
+                        min_index = index_1
+                    temp_list = original_solution[min_index:max_index]
+                    np.random.shuffle(temp_list)
+                    _solution[min_index:max_index] = temp_list
+                elif self.mutation_type == "single bitflip":
+                    numbers = list(range(0, self.dimensions))
+                    index_1 = random.choice(numbers)
+                    temp = _solution[index_1]
+                    if temp == 0:
+                        _solution[index_1] = 1
+                    elif temp ==1:
+                        _solution[index_1] = 0
+                elif self.mutation_type == "multi bitflip":
+                    for i, x in enumerate(_solution):
+                        if random.random() < self.mutation_rate:
+                            if x == 0:
+                                _solution[i] = 1
+                            elif x == 1:
+                                _solution[i] = 0
         return _solution
 
     def crossover(self, first_solution, second_solution):
@@ -121,7 +126,9 @@ class GA:
         Single = Single point crossover, a random index is selected and the parents are crossed based on this index.
         Multi = Multi-point crossover, two indices are selected and the values between these indices are swapped between
                 the parents.
-
+        Uniform = For each variable, swap variable value between parents if crossover rate is met.
+        Blend boolean = Crossover for continuous variable space: take the average of the two parents' variable values.
+        This can be applied to any of the three above methods.
         @return: The crossed over children, or an empty array if crossover was not performed.
         """
         if random.random() < self.crossover_rate:
@@ -137,13 +144,32 @@ class GA:
                     max_index = index_2
                     min_index = index_1
                 max_index = max_index + 1
-                _first_solution[min_index:max_index] = second_solution[min_index:max_index]
-                _second_solution[min_index:max_index] = first_solution[min_index:max_index]
+                if self.continuous_bool:
+                    third_solution = [(x + y)/2 for (x, y) in zip(first_solution, second_solution)]
+                    _first_solution[min_index:max_index] = third_solution[min_index:max_index]
+                    _second_solution[0:min_index] = third_solution[0:min_index]
+                    _second_solution[max_index:-1] = third_solution[max_index:-1]
+                if not self.continuous_bool:
+                    _first_solution[min_index:max_index] = second_solution[min_index:max_index]
+                    _second_solution[min_index:max_index] = first_solution[min_index:max_index]
             elif self.crossover_type == 'single':
                 index_1 = random.randint(0, self.dimensions - 1)
-                _first_solution[0:index_1] = second_solution[0:index_1]
-                _second_solution[index_1:-1] = first_solution[index_1:-1]
-
+                if self.continuous_bool:
+                    third_solution = [(x + y)/2 for (x, y) in zip(first_solution, second_solution)]
+                    _first_solution[0:index_1] = third_solution[0:index_1]
+                    _second_solution[index_1:-1] = third_solution[index_1:-1]
+                if not self.continuous_bool:
+                    _first_solution[0:index_1] = second_solution[0:index_1]
+                    _second_solution[index_1:-1] = first_solution[index_1:-1]
+            elif self.crossover_type == 'uniform':
+                for i in range(len(first_solution)):
+                    if random.random() < self.crossover_rate:
+                        if self.continuous_bool:
+                            _first_solution[i] = (first_solution[i] + second_solution[i])/2
+                            _second_solution[i] = (first_solution[i] + second_solution[i])/2
+                        else:
+                            _first_solution[i] = second_solution[i]
+                            _second_solution[i] = first_solution[i]
             return [_first_solution, _second_solution]
         else:
             return []

refactoring/utilities/field/field_creation.py
@@ -531,7 +531,7 @@ class Field:
 
         return expected_bin_strat, max_strat, min_strat
 
-    def create_strip_groups(self):
+    def create_strip_groups(self, overlap=False, overlap_ratio=0.1):
         cell_indeces = self.create_strip_trial()
         factors = []
         single_cells = []
@@ -544,6 +544,21 @@ class Field:
             sum = sum + len(strip.original_index)
         if single_cells:
             factors.append(single_cells)
+        if overlap:
+            new_factors = []
+            nr_of_cells = [np.ceil(len(f) * overlap_ratio) for f in factors]
+            for i, f in enumerate(factors):
+                if i != len(factors) -1:
+                    nf = []
+                    for j in range(1, nr_of_cells[i]+1):
+                        print(j)
+                        nf.append(f[-j])
+                    for j in range(nr_of_cells[i+1]):
+                        print(j)
+                        nf.append(f[j])
+                    new_factors.append(nf)
+            factors.extend(new_factors)
+        print(factors)
         return factors
 
 